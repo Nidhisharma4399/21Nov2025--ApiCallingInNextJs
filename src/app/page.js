3d186082c19d462a3e948102bd36e922c2d9dc0a
Nidhi Sharma <nidhibharat399@gmail.com>
2025-11-21 16:51:07 +0530

    Nextjs Api Calling

--- src/app/page.js
+++ src/app/page.js
@@ -12,37 +12,37 @@ export default function Home() {
       <div className="row g-3 justify-content-center">
 
         <div className="col-md-4">
-          <Link href="/posts" className="btn btn-primary w-100 py-3">
+          <Link href="/posts" target="_blank" className="btn btn-primary w-100 py-3">
             Posts
           </Link>
         </div>
 
         <div className="col-md-4">
-          <Link href="/comments" className="btn btn-success w-100 py-3">
+          <Link href="/comments" target="_blank" className="btn btn-success w-100 py-3">
             Comments
           </Link>
         </div>
 
         <div className="col-md-4">
-          <Link href="/albums" className="btn btn-warning w-100 py-3">
+          <Link href="/albums" target="_blank" className="btn btn-warning w-100 py-3">
             Albums
           </Link>
         </div>
 
         <div className="col-md-4">
-          <Link href="/photos" className="btn btn-info w-100 py-3">
+          <Link href="/photos" target="_blank" className="btn btn-info w-100 py-3">
             Photos
           </Link>
         </div>
 
         <div className="col-md-4">
-          <Link href="/todos" className="btn btn-secondary w-100 py-3">
+          <Link href="/todos" target="_blank" className="btn btn-secondary w-100 py-3">
             Todos
           </Link>
         </div>
 
         <div className="col-md-4">
-          <Link href="/users" className="btn btn-dark w-100 py-3">
+          <Link href="/users"  target="_blank" className="btn btn-dark w-100 py-3">
             Users
           </Link>
         </div>
