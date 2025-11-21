7f4206803cd964fe0eb2592075c9727398bd52a9
Nidhi Sharma <nidhibharat399@gmail.com>
2025-11-21 16:43:02 +0530

    Api Calling Through NextJs

--- src/app/page.js
+++ src/app/page.js
@@ -1,66 +1,56 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+    <div className="container py-5">
+      <h1 className="text-center mb-4">Welcome</h1>
+
+      <div className="row g-3 justify-content-center">
+
+        <div className="col-md-4">
+          <Link href="/posts" className="btn btn-primary w-100 py-3">
+            Posts
+          </Link>
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+        <div className="col-md-4">
+          <Link href="/comments" className="btn btn-success w-100 py-3">
+            Comments
+          </Link>
         </div>
-      </main>
+
+        <div className="col-md-4">
+          <Link href="/albums" className="btn btn-warning w-100 py-3">
+            Albums
+          </Link>
+        </div>
+
+        <div className="col-md-4">
+          <Link href="/photos" className="btn btn-info w-100 py-3">
+            Photos
+          </Link>
+        </div>
+
+        <div className="col-md-4">
+          <Link href="/todos" className="btn btn-secondary w-100 py-3">
+            Todos
+          </Link>
+        </div>
+
+        <div className="col-md-4">
+          <Link href="/users" className="btn btn-dark w-100 py-3">
+            Users
+          </Link>
+        </div>
+
+        
+
+      </div>
     </div>
+    </>
   );
 }
